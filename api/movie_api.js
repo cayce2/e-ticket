19ce1d79484cc160cbabf4aea555fca37f256360
Nick Munene <nickmunene101@gmail.com>
2023-02-21 23:31:52 +0300

    Folder structure and included axios script

---
+++ api/movie_api.js
@@ -0,0 +1,6 @@
+
+const movies_in_theatre = "/discover/movie?primary_release_date.gte=2014-09-15&primary_release_date.lte=2014-10-22";
+const popular_movies = "/discover/movie?sort_by=popularity.desc";
+async function get_latest_movie() {
+    return 0;
+}
---
+++ config/config.js
@@ -0,0 +1,4 @@
+const config = {
+    API_KEY: "ba6145923d7d14647e463c395918655f",
+    BASE_URL: "https://api.themoviedb.org/3/"
+}
